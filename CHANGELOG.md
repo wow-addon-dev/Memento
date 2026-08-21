9288cd0c6c4c7b55af7a5d420f9c454c2515c545
Dionys <72759855+diomsg-code@users.noreply.github.com>
2026-08-21 21:53:19 +0200

    v2.25

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,7 +2,4 @@
 ### [@project-version@](https://github.com/wow-addon-dev/Memento/tree/@project-version@) (@build-date@)
 [Full Changelog](@full-changelog@) - [Previous Releases](https://github.com/wow-addon-dev/Memento/releases)
 
-- Added: Changelog window available from the options menu
-- Added: Changelog window available through the 'changelog' slash command
-- Removed: Version notice chat messages
-- Adapted to the latest version of Arcane Wizard: Library to ensure full compatibility
+- No changes available
--- data/ChangelogData.lua
+++ data/ChangelogData.lua
@@ -8,6 +8,13 @@ MEM.CHANGELOG = {
 		version = version,
 		date = buildDate ~= "" and buildDate or nil,
 		entries = {
+			"No changes available"
+		}
+	},
+	{
+		version = "v2.24",
+		date = "2026-08-18",
+		entries = {
 			"Added: Changelog window available from the options menu",
 			"Added: Changelog window available through the 'changelog' slash command",
 			"Removed: Version notice chat messages",
@@ -46,6 +53,36 @@ MEM.CHANGELOG = {
 			"Removed: TOC version for patch 1.15.8 [classic]",
 			"Minor code adjustments",
 			"Adapted to the latest version of Arcane Wizard: Library to ensure full compatibility"
+		}
+	},
+	{
+		version = "v2.19",
+		date = "2026-07-26",
+		entries = {
+			"Added: Configurable sound effects can now be played after a screenshot has been taken"
+		}
+	},
+	{
+		version = "v2.18",
+		date = "2026-07-21",
+		entries = {
+			"Updated: deDE, enUS localizations"
+		}
+	},
+	{
+		version = "v2.17",
+		date = "2026-07-18",
+		entries = {
+			"Minor code adjustments"
+		}
+	},
+	{
+		version = "v2.16",
+		date = "2026-07-12",
+		entries = {
+			"Added: Wago project page button",
+			"Removed: TOC version for patch 5.5.3 [mists of pandaria - classic]",
+			"Removed: TOC version for patch 2.5.5 [burning crusade - classic anniversary edition]"
 		}
 	}
 }
